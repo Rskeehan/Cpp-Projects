658774476eb00c1b5fbd1680b292b9b2ae427c11
rob <rskeehan@gmail.com>
2022-11-26 20:40:40 -0600

    Added MyString Class to HW5 and added input file

--- Constance Projects/Project5/HW5.cpp
+++ Constance Projects/Project5/HW5.cpp
@@ -6,8 +6,46 @@
 
 using namespace std;
 
-int main() 
+class MyString
 {
-    //Code here
+private:
+    char content[32];
+    int length;
+
+public:
+    MyString() // default constructor
+    {
+        length = 0;
+        content[32] = {};
+    };
+
+    // getters
+    int getLength() // returns length of private member var 'content'
+    {
+        return length;
+    };
+
+    bool pushBack(char inchar) // adds a char to the 'content' char stack if possible`
+    {
+        bool successfullyAdded;
+        if (length >= 32)
+        {
+            successfullyAdded = false;
+        }
+        else
+        {
+            content[length] = inchar; // adds character to top of stack
+            length += 1;              // tracks top of content stack
+
+            successfullyAdded = true;
+        }
+
+        return successfullyAdded; // return result
+    }
+};
+
+int main()
+{
+    // Code here
     return EXIT_SUCCESS;
-}
+};
